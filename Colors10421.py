14d3582faa8cfc4c7426fbc3fc5099a661b1725b
jissa7 <91565766+jissa7@users.noreply.github.com>
2021-10-04 15:02:32 -0500

    Add files via upload

---
+++ Colors10421.py
@@ -0,0 +1,14 @@
+#Initialize Turtle
+import turtle
+import random
+jenna=turtle.Turtle()
+
+#Functions
+def randomDot():
+    jenna.color("pink")
+    jenna.begin_fill()
+    jenna.circle(90)
+    jenna.end_fill()
+    
+#Main
+randomDot()
