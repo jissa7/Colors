ae5a5d93bd02ac79f65249080f05a06d733ab5bb
jissa7 <91565766+jissa7@users.noreply.github.com>
2021-10-06 14:53:13 -0500

    Update Colors10421.py

--- Colors10421.py
+++ Colors10421.py
@@ -2,13 +2,43 @@
 import turtle
 import random
 jenna=turtle.Turtle()
+turtle.colormode(255)
+jenna.speed(100)
 
 #Functions
 def randomDot():
-    jenna.color("pink")
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    s=random.randint(0,300)
+    jenna.color(r,g,b)
     jenna.begin_fill()
-    jenna.circle(90)
+    jenna.circle(s)
+    jenna.end_fill()
+    print(r,g,b)
+def drawSquare():
+    s=random.randint(0,300)
+    for i in range (4):
+        jenna.forward(s)
+        jenna.right(90)
+def randomSquare():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    jenna.color(r,g,b)
+    jenna.begin_fill()
+    drawSquare()
     jenna.end_fill()
     
 #Main
-randomDot()
+for i in range (500):
+    x=random.randint(-500,500)
+    y=random.randint(-500,500)
+    jenna.penup()
+    jenna.goto(x,y)
+    jenna.pendown()
+    randomDot()
+    jenna.penup()
+    jenna.goto(x,y)
+    jenna.pendown()
+    randomSquare()
